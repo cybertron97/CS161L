129298320e212bf1964642465006b42107fbdc19
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-04-15 20:15:29 -0700

    Add files via upload

---
+++ Lab2/fixedfloat.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:07:18 04/15/2019 
+// Design Name: 
+// Module Name:    fixedfloat 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fixedfloat(
+    input clk,
+    input rst,
+    input targetnumber,
+    input fixpointpos,
+    input opcode,
+    output result
+    );
+
+
+endmodule
