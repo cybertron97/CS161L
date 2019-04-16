a69106a83ebb8f25787c348b504e4d0e7e212b2c
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-04-15 20:16:49 -0700

    Add files via upload

--- Lab2/fixedfloat.v
+++ Lab2/fixedfloat.v
@@ -19,13 +19,54 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module fixedfloat(
-    input clk,
-    input rst,
-    input targetnumber,
-    input fixpointpos,
-    input opcode,
-    output result
+    input wire clk,
+    input wire rst,
+    input wire[31:0] targetnumber,
+    input wire[4:0] fixpointpos,
+    input wire opcode,
+    output wire[31:0] result
     );
+wire [31:0] floatresult; 
+wire [31:0] fixresult; 
+wire [31:0] targetnumber_b;
+wire [4:0] fixpointpos_c;
 
+integer i; 
+integer count; 
+reg sign =0; 
+reg [7:0] exp; 
+integer exp2;
+reg[22:0]mantissa;
+reg [31:0] temp; 
+
+always @(*) begin 
+targetnumber_b= targetnumber; 
+
+
+// -------------------------------------------
+// From fix to float (Part 1)
+// -------------------------------------------
+// Your  Implementation 
+
+case (opcode) 
+	1'b00: begin 
+	
+
+
+
+// -------------------------------------------	
+// From float to fix (Part 2)
+// -------------------------------------------
+// Your  Implementation  
+
+// -------------------------------------------	
+// Register the results 
+// -------------------------------------------
+
+always @ ( posedge clk ) begin 
+    // Synchronous reset
+    result <= opcode == 1 ?  fixresult : floatresult ;
+
+end 
 
 endmodule
