045e79afe6e51b696c048e1525d79af7c889b102
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-04-15 20:50:22 -0700

    Add files via upload

--- Lab2/fixedfloat.v
+++ Lab2/fixedfloat.v
@@ -41,7 +41,9 @@ reg [31:0] temp;
 
 always @(*) begin 
 targetnumber_b= targetnumber; 
-
+fixpointpos_c = fixpointpos; 
+float_r= 32'h0;
+fix_r=32'h0; 
 
 // -------------------------------------------
 // From fix to float (Part 1)
@@ -49,15 +51,60 @@ targetnumber_b= targetnumber;
 // Your  Implementation 
 
 case (opcode) 
-	1'b00: begin 
+	1'b00: 
+begin 
+	if (targetnumber_b[31]==1)
+	begin 
+		sign= 1'b1; 
+		targetnumber_b=-targetnumber_b; 
+	end 
+	for (i=0; i<31; i=i+1) 
+	begin
+		if(targetnumber[i]==1) 
+		begin 
+			count =i; 
+		end
+	end 
+	exp = (count - fixpointpos_c) +127; 
 	
-
-
-
+	targetnumber_b= targetnumber_b <<(31-count); 
+	mantissa = targetnumber_b [31:8] ; 
+	 floatresult = {{sign},{exp},{mantissa}}; 
+end 
 // -------------------------------------------	
 // From float to fix (Part 2)
 // -------------------------------------------
 // Your  Implementation  
+
+1'b01:
+begin 
+ sign = targetnumber_b[31]; 
+ exp2= targetnumber_b[30:23]-127; 
+ mantissa = targetnumber_b[22:0];
+ count= 23-(exp2+fixpointpos_c); 
+  if (count>0)
+  begin 
+	temp = temp >> count; 
+  end 
+  else if (count <=0)
+  begin 
+	temp = temp<<-count ;
+  end 
+  if (sign == 1'b1)
+  begin 
+	temp=-temp;
+  end 
+  
+  fixresult= temp; 
+  
+  end 
+  
+ endcase
+ end
+	
+
+
+
 
 // -------------------------------------------	
 // Register the results 
@@ -65,6 +112,10 @@ case (opcode)
 
 always @ ( posedge clk ) begin 
     // Synchronous reset
+	 if (rst==1'b1)
+	 begin 
+		result <= 32'h0; 
+	 end
     result <= opcode == 1 ?  fixresult : floatresult ;
 
 end 
