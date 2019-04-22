ab58dd41d6756af4883226eb4a2c30ac01ce8af6
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-04-21 17:14:11 -0700

    Update test_float_tb.v

--- Lab2/test_float_tb.v
+++ Lab2/test_float_tb.v
@@ -57,25 +57,73 @@ module test_float_tb;
    end 	  
 
 initial begin 
+//Initial
 opcode = 1'b00;
 fixpointpos= 4'b0000; 
 targetnumber = {32'h0}; 
 R = {32'h0}; 
 #100; 
 
-
-opcode = 1'b00; 
-fixpointpos = 5'b11001; 
-targetnumber = {{12'b0011001_01000}, {20'h00000}} ;
-R = 32'b0_10000011_10010100000000000000000;
-#100; 
-$display("TC10.... %h %h", R,result); 
-if(R!=result) $display("Failed TC10...%h %h", R , result);
-		// Wait 100 ns for global reset to finish
-		#100;
-        
-		// Add stimulus here
-
+	//Testcase1
+	opcode = 1'b00; 
+	fixpointpos = 5'b11001; 
+	targetnumber = {{12'b0011001_01000}, {20'h00000}} ;
+	R = 32'b0_10000011_10010100000000000000000;
+	// Wait 100 ns for global reset to finish
+	#100; 
+	$display("TC10 Fix to float", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);	
+	//Testcase2
+        fixpointpos = 4'b0011; 
+	targetnumber= {{24'h000000}, {8'b01100_101}}; 
+	R = 32'b0__10000010_10010100000000000000000;
+	#100;
+	$display("TC11 Fix to float", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	//Testcase3
+	fixpointpos = 4'b0100;
+	targetnumber = { {24'h000000}, {8'b0110_0101}  } ; 
+	R = 32'b0_10000001_10010100000000000000000;
+	#100;
+	$display("TC12 Fix to float", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	//Testcase4  512
+	fixpointpos = 5'b0001;
+	targetnumber = {{20'h00000}, {12'h400}}; 
+	R = 32'b0_10001000_00000000000000000000000;
+	#100;
+	$display("TC13 Fix to float", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	
+	//Float to fix
+	opcode = 1'b01; 
+	//Testcase 01 
+	fixpointpos = 5'b11001; 
+	targetnumber= 32'b0_10000011_10010100000000000000000;
+	R  = {{12'b0011001_01000}, {20'h00000}};
+	#100; 
+	$display("TC20 Float to fix", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	// TestCase 02 
+	fixpointpos = 4'b0011;
+	targetnumber = 32'b0_10000010_10010100000000000000000; 
+	R = {{24'h000000},{8'b01100_101}};
+	#100;
+	$display("TC21 Float to fix", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	//testcase03 
+	fixpointpos = 4'b0100;
+	targetnumber = 32'b0_10000001_10010100000000000000000;
+	R = {{24'h000000},{8'b0110_0101}}; 
+	#100;
+	$display("TC22 Float to fix", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
+	//testcase04 
+	fixpointpos = 5'b0001;
+	targetnumber = 32'b0_10001000_00000000000000000000000 ;
+	R = {{20'h00000},{12'h400}} ;  
+	$display("TC23 Float to fix", R,result); 
+	if(R!=result) $display("Result is wrong", R , result);
 	end
       
 endmodule
